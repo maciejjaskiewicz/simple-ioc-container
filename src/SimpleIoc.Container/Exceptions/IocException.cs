905f444802665dbc17a181e9f670079eb6a0b26d
Maciej Jaśkiewicz <maciek.jaskiewicz97@gmail.com>
2017-06-09 18:47:50 +0200

    added basic exceptions

---
+++ src/SimpleIoc.Container/Exceptions/IocException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimpleIoc.Container.Exceptions
+{
+    public abstract class IocException : Exception
+    {
+        public Type Type { get; }
+
+        public IocException(Type type)
+        {
+            Type = type;
+        }
+    }
+}
