41723cb218b3d4ac37bdfbe33445c94b90b843af
Maciej Jaśkiewicz <maciek.jaskiewicz97@gmail.com>
2017-06-09 17:55:30 +0200

    added test for basic register and resolve operations

---
+++ tests/SimpleIoc.Tests/IocContainerTests.cs
@@ -0,0 +1,35 @@
+﻿using Shouldly;
+using SimpleIoc.Container;
+using SimpleIoc.Tests.Resources;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SimpleIoc.Tests
+{
+    public class IocContainerTests
+    {
+        private readonly IocContainer _container;
+
+        public IocContainerTests()
+        {
+            _container = new IocContainer();
+        }
+
+        private ITestInterface act()
+        {
+            return _container.Resolve<ITestInterface>();
+        }
+
+        [Fact]
+        public void given_registered_interface_should_resolve_implementation()
+        {
+            _container.Register<ITestInterface, TestImplementation>();
+
+            var resolveObject = act();
+
+            resolveObject.ShouldBeOfType<TestImplementation>();
+        }
+    }
+}
