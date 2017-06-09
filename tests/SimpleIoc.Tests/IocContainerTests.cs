714bfc39dd0d7ceaa1f985ab9053ec7ce1fbc1ae
Maciej Jaśkiewicz <maciek.jaskiewicz97@gmail.com>
2017-06-09 18:46:42 +0200

    added test for handle components with multpile ctors

--- tests/SimpleIoc.Tests/IocContainerTests.cs
+++ tests/SimpleIoc.Tests/IocContainerTests.cs
@@ -1,5 +1,6 @@
 ﻿using Shouldly;
 using SimpleIoc.Container;
+using SimpleIoc.Container.Exceptions;
 using SimpleIoc.Tests.Resources;
 using Xunit;
 
@@ -32,12 +33,23 @@ namespace SimpleIoc.Tests
         [Fact]
         public void given_registered_interface_should_resolve_implementation_with_dependency()
         {
-            _container.Register<ITestInterface, TestImplementationWithDependency>();
             _container.Register<Dependency, Dependency>();
+            _container.Register<ITestInterface, TestImplementationWithDependency>();
 
             var resolveObject = act();
 
             resolveObject.ShouldBeOfType<TestImplementationWithDependency>();
+        }
+
+        [Fact]
+        public void throws_when_component_has_multiple_ctors()
+        {
+            _container.Register<Dependency, Dependency>();
+            _container.Register<ITestInterface, TestImplementationWithMultipleCtors>();
+
+            var exception = Record.Exception(() => act());
+
+            exception.ShouldBeOfType<ComponentHasMultipleConstructorsException>();
         }
     }
 }
